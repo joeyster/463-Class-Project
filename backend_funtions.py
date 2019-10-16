25f6b6bca56cb4ff7110d5502eae276cce2490d4
RobertRivas <r_rivas@csu.fullerton.edu>
2019-10-16 13:21:05 -0700

    added backend functions file.

---
+++ backend_funtions.py
@@ -0,0 +1,14 @@
+def create_warehouse(length, width):
+
+    # todo: get user warehouse size in form from button?
+
+
+    return length * width
+
+
+def edit_warehouse_size(length, width):
+    new_length = length
+    new_width = width
+
+
+    return new_length * new_width
