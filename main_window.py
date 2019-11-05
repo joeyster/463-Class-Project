60eb7b41293798b6f7648257cb22226d90f29f7d
Maxw21 <Maxw21@users.noreply.github.com>
2019-11-04 17:14:06 -0800

    Error Display, Input Sanitization, Repo Cleanup

---
+++ main_window.py
@@ -0,0 +1,19 @@
+from PyQt5.QtWidgets import QMainWindow
+from ui_widget import UIWidget
+
+
+class MainWindow(QMainWindow):
+    """Overwrite the main window of PyQt for pygame integration."""
+    def __init__(self, surface, controller, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.ui_widget = UIWidget(surface=surface, controller=controller, window=self)
+        self.setCentralWidget(self.ui_widget)
+        self.setWindowTitle('The Warehouse')
+
+    def update_image(self, surface):
+        """Update the image in the pygame window."""
+        self.ui_widget.update_image(surface=surface)
+
+    def display_error(self, message):
+        """Pass the error message to the widget."""
+        self.ui_widget.display_error(message=message)
