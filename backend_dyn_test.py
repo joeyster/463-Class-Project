25c735adf7f903a8da678190a8288473b8f56745
RobertRivas <r_rivas@csu.fullerton.edu>
2019-11-06 20:36:48 -0800

    add item  unit test

--- backend_dyn_test.py
+++ backend_dyn_test.py
@@ -1,2 +1,18 @@
-import unittest
 
+from warehouse import Warehouse
+from item import Item
+import unittest 
+
+warehouse = Warehouse()
+
+class TestStringMethods(unittest.TestCase):
+
+
+    def add_item_test(self):
+        test_list = ['123', 'apples', 5, 6, 6]
+        warehouse.add_item(test_list)
+        self.assertEqual('apples', warehouse.new_item_list[0].name)
+
+        
+if __name__ == '__main__':
+    unittest.main()
