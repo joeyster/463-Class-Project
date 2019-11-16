9a24f06daf42848f2c406a503cd1ebbf51c42aad
Maxw21 <Maxw21@users.noreply.github.com>
2019-11-15 22:22:49 -0800

    Add dynamic backend tests for warehouse

--- backend_dyn_test.py
+++ backend_dyn_test.py
@@ -21,16 +21,56 @@ class MyTestCase(unittest.TestCase):
 
     def test_remove_item(self):
         warehouse = Warehouse(True)
-        # test_list = [123, 'apples', 5, 6, 6]
-        #  warehouse.remove_item(test_list)
+        test_list = [123, 'apples', 5, 6, 6]
+        warehouse.add_item(test_list)
+        warehouse.remove_item(test_list[0])
         self.assertEqual([], warehouse.item_list, 'Passed')
 
+    def test_change_warehouse_size(self):
+        warehouse = Warehouse(True)
+        warehouse.change_warehouse_size(10, 5)
+        self.assertEqual(10, warehouse.board_width, 'Passed')
+        self.assertEqual(5, warehouse.board_height, 'Passed')
 
-# todo: locate item
-# todo: change warehouse size
-# todo: pack Warrehouse
-# todo: check available area
-# todo: packing
+    def test_init_board(self):
+        warehouse = Warehouse(True)
+        warehouse.board_width = 10
+        warehouse.board_height = 5
+        warehouse.init_board()
+        self.assertEqual(10, len(warehouse.new_board[0]), 'Passed')
+        self.assertEqual(5, len(warehouse.new_board), 'Passed')
+
+    def test_check_available_area(self):
+        warehouse = Warehouse(True)
+        test_list = [123, 'apples', 1, 6, 6]
+        warehouse.add_item(test_list)
+        test_list_2 = [456, 'oranges', 2, 2, 2]
+        warehouse.add_item(test_list_2)
+        test_true = warehouse.check_available_area(warehouse.new_item_list, 44)
+        test_false = warehouse.check_available_area(warehouse.new_item_list, 10)
+        self.assertEqual(True, test_true, 'Passed')
+        self.assertEqual(False, test_false, 'Passed')
+
+    def test_fill_board(self):
+        warehouse = Warehouse(True)
+        warehouse.board_width = 10
+        warehouse.board_height = 10
+        warehouse.init_board()
+        test_item = [123, 'apples', 1, 6, 6]
+        warehouse.add_item(test_item)
+        warehouse.fill_board(0, warehouse.new_item_list[0], 1)
+        self.assertEqual('1', warehouse.new_board[0][0], 'Passed')
+
+    def test_packing(self):
+        warehouse = Warehouse(True)
+        warehouse.board_width = 10
+        warehouse.board_height = 10
+        warehouse.init_board()
+        test_item = [123, 'apples', 1, 6, 6]
+        warehouse.add_item(test_item)
+        self.assertEqual(0, len(warehouse.complete_list), 'Passed')
+        warehouse.packing(warehouse.new_item_list)
+        self.assertEqual(1, len(warehouse.complete_list), 'Passed')
 
 
 if __name__ == '__main__':
